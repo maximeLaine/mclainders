bf7c6f8deec34f9118d7db8b528bc18e73eeeb0d
maximeLaine <mlaine@live.fr>
2026-01-18 20:05:07 +0100

    Fix bug for deployement

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import FallbackPage from "./pages/FallbackPage";
 import "./index.css";
 
-// Try to render the app, fall back to a simple page if there's an error
-try {
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} catch (error) {
-  // Render a simple fallback page if app fails to initialize
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <FallbackPage />
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
